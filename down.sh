952455e10128d6db47bd3eef9036ea241a2be613
Hicham AIT TALGHALIT <hicham.aittalghalit@gmail.com>
2022-03-19 11:06:29 +0100

    first commit

---
+++ down.sh
@@ -0,0 +1,2 @@
+docker-compose stop mysql workspace php-worker nginx php-fpm
+
---
+++ reload.sh
@@ -0,0 +1,4 @@
+docker-compose stop mysql workspace php-worker nginx php-fpm
+
+docker-compose up -d mysql workspace php-worker nginx php-fpm
+
